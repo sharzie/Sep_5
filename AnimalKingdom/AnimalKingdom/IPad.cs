04ee4d4fa802adc1534d9fea72510a6f44d6669f
sharzie <doctorghanti@gmail.com>
2015-09-05 07:50:06 +0530

    initial commit

---
+++ AnimalKingdom/AnimalKingdom/IPad.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalKingdom
+{
+    public interface IPad
+    {
+        string this[string s] { get; set; }
+        event EventHandler<EventArgs> RangeExceeded;
+    }
+}
---
+++ AnimalKingdom/AnimalKingdom/Pad.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalKingdom
+{
+    public class Pad : IPad
+    {
+        Dictionary<string, string> MyDic = new Dictionary<string, string>();
+        public event EventHandler<EventArgs> RangeExceeded;
+        public string this[string s] 
+        {
+            get
+            {
+                return MyDic[s];
+            }
+            set
+            {
+                MyDic[s] = value;
+            }
+        }
+    }
+}
